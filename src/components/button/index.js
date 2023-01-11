c36ba0461a12324df48b7697b1ff5d857cac443e
tzhovtyi <tzhovtyy@mail.ru>
2023-01-12 00:10:54 +1000

    Code refactoring

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,11 +1,6 @@
 import Handlebars from 'handlebars';
 import tpl from 'bundle-text:./tpl.hbs';
 import './style.scss';
-Handlebars.registerHelper('stringifyFunc', function(fn) {
-    return new Handlebars.SafeString("(" + 
-               fn.toString().replace(/\"/g,"'") + ")()");
-});
-
 
 export default (label, func) => {
 	return Handlebars.compile(tpl)({label: label, func: func});
